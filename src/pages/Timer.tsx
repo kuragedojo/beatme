542275c70a250e1de8dcb759ac642397328012a8
Christof Bachmann <c.bachmann@pro54.com>
2021-06-03 17:59:02 +0200

    Add visual progress circle

--- src/pages/Timer.tsx
+++ src/pages/Timer.tsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { RefresherEventDetail } from '@ionic/core';
 import './Timer.css';
 import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonRefresher, IonRefresherContent, IonContent } from '@ionic/react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 enum TimerPhase {
   START,
@@ -14,6 +16,7 @@ enum TimerPhase {
 
 type TimerState = {
   time: number,
+  maxTime: number,
   round: number,
   set: number,
   phase: TimerPhase,
@@ -35,6 +38,7 @@ export class Timer extends Component<TimerProps, TimerState> {
 
     this.state = {
       time: props.warmUpTime,
+      maxTime: props.warmUpTime,
       round: props.rounds,
       set: props.sets,
       phase: TimerPhase.START,
@@ -78,8 +82,45 @@ export class Timer extends Component<TimerProps, TimerState> {
     }
   }
 
+  getPhaseColorPath() {
+    if (this.state.phase === TimerPhase.START) {
+      return '#000';
+    }
+    if (this.state.phase === TimerPhase.WARMUP) {
+      return '#aff';
+    }
+    if (this.state.phase === TimerPhase.WORKOUT) {
+      return '#afa';
+    }
+    if (this.state.phase === TimerPhase.REST) {
+      return '#ffa';
+    }
+    if (this.state.phase === TimerPhase.END) {
+      return '#fff';
+    }
+  }
+
+  getPhaseColorText() {
+    if (this.state.phase === TimerPhase.START) {
+      return '#000';
+    }
+    if (this.state.phase === TimerPhase.WARMUP) {
+      return '#8dd';
+    }
+    if (this.state.phase === TimerPhase.WORKOUT) {
+      return '#8d8';
+    }
+    if (this.state.phase === TimerPhase.REST) {
+      return '#dd8';
+    }
+    if (this.state.phase === TimerPhase.END) {
+      return '#fff';
+    }
+  }
+
   tick() {
     let newTime = this.state.time - 1;
+    let newMaxTime = this.state.maxTime;
     let newPhase = this.state.phase;
     let newRound = this.state.round;
     let newSet = this.state.set;
@@ -88,20 +129,23 @@ export class Timer extends Component<TimerProps, TimerState> {
       newPhase = TimerPhase.WORKOUT;
     }
 
-    if (newTime === 0) {
+    if (newTime < 0) {
       if (this.state.phase === TimerPhase.WARMUP) {
         newPhase = TimerPhase.WORKOUT;
         newTime = this.props.workoutTime;
+        newMaxTime = newTime;
       }
 
       if (this.state.phase === TimerPhase.WORKOUT) {
         newPhase = TimerPhase.REST;
         newTime = this.props.restTime;
+        newMaxTime = newTime;
       }
 
       if (this.state.phase === TimerPhase.REST) {
         newPhase = TimerPhase.WARMUP;
         newTime = this.props.warmUpTime;
+        newMaxTime = newTime;
         if (newSet === 1) {
           if (newRound === 1) {
             newPhase = TimerPhase.END;
@@ -118,6 +162,7 @@ export class Timer extends Component<TimerProps, TimerState> {
 
     this.setState({
       time: newTime,
+      maxTime: newMaxTime,
       phase: newPhase,
       round: newRound,
       set: newSet
@@ -138,10 +183,22 @@ export class Timer extends Component<TimerProps, TimerState> {
           <IonCard>
             <IonCardHeader>
               <IonCardTitle>Test-Timer</IonCardTitle>
-              <IonCardSubtitle><strong>Time:</strong> {this.state.time}</IonCardSubtitle>
+              <IonCardSubtitle>Nur zum Testen</IonCardSubtitle>
             </IonCardHeader>
 
             <IonCardContent>
+            <CircularProgressbar 
+            value={this.state.maxTime - this.state.time} 
+            maxValue={this.state.maxTime} 
+            text={this.state.time+'s'}
+            styles={buildStyles({
+              // Colors
+              pathColor: this.getPhaseColorPath(),
+              textColor: this.getPhaseColorText(),
+              trailColor: '#eee',
+              backgroundColor: '#3e98c7',
+            })}
+             />
               <strong>Runde:</strong> {this.state.round}/{this.props.rounds}<br />
               <strong>Set:</strong> {this.state.set}/{this.props.sets}<br />
               <strong>Phase:</strong> {this.getPhaseText()}<br />
