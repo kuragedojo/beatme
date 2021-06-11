940a8b4d2e2b68ac81895b9f2be5db8c6dce25ef
Christof Bachmann <c.bachmann@pro54.com>
2021-06-11 20:24:43 +0200

    Add capacitor build config

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'info.jpr0g.beatme',
+  appName: 'beatMe',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
