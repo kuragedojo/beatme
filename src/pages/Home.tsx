978fd3e009690cd3743b876eaddfd427df86b9e2
Christof Bachmann <c.bachmann@pro54.com>
2021-06-11 19:49:17 +0200

    Add slider to home screen for multiple timers

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,28 @@ import React from 'react';
 import {
   IonFab,
   IonFabButton,
-  IonIcon
+  IonIcon,
+  IonSlide,
+  IonSlides
 } from '@ionic/react';
 
 const Home: React.FC = () => {
+
+  const slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+
   return (
     <Layout>
-      <Timer warmUpTime={2} restTime={3} workoutTime={5} rounds={2} sets={3} />
+      <IonSlides pager={true} options={slideOpts}>
+        <IonSlide>
+          <Timer warmUpTime={2} restTime={3} workoutTime={5} rounds={2} sets={3} />
+        </IonSlide>
+        <IonSlide>
+          <Timer warmUpTime={10} restTime={30} workoutTime={240} rounds={4} sets={2} />
+        </IonSlide>
+      </IonSlides>
       <IonFab vertical="bottom" horizontal="center" slot="fixed" >
         <IonFabButton href="/home">
           <IonIcon icon={timer} />
